cdbe4902f29a55cabe49cc97eb2f42fae158b0d7
Dan <dcolish@gmail.com>
2010-06-01 07:12:10 -0700

    updated some lemmas

--- coqext/hypreason/Tactics.v
+++ coqext/hypreason/Tactics.v
@@ -140,12 +140,14 @@ P_with_CP. P_with_CP. DS H1 H2. Qed.
 
 
 Ltac Pose D For N :=
-  match D with
-    | ~N =>
-      destruct(excluded_middle N); [try right; try assumption |]
-    | ~D => idtac "You cannot pose that" D
-    | _ => idtac "Not matching" D N
-  end.
+  let H := fresh "H0" in 
+    let H' := fresh "H0" in 
+      match D with
+        | ~N =>
+          destruct(excluded_middle N) as [H | H'] ; [try right; try assumption |]
+            | ~D => idtac "You cannot pose that" D
+            | _ => idtac "Not matching" D N
+      end.
 
 Ltac Contr A B:= 
   try apply A in B; contradiction || apply B in A; contradiction ||
@@ -192,15 +194,18 @@ Ltac Simp_right C :=
    | _ => idtac "[insert appropriate error message]"
    end.
 
-Lemma MT (A B:Prop): (A -> B) -> (~B -> ~A).  
-  Universal_Intros.
-  P_with_CP.
-  P_with_CP.
-  Pose(~~A) For (~A). 
-  DN H.
-  MP H1 H.
-  Contr H2 H3.
-  Qed.
+Ltac MT H N := 
+  let C := fresh "H0" in
+    let P := type of H in  
+      match P with
+        | ?p -> ?q => 
+          match type of N with
+            | ~q => assert (C: ~q -> ~p); intros; [tauto |] ; apply C in N; clear C
+            | ?T => idtac T "is not a negation of " q
+          end
+        | ?T => idtac T "was expected to be an implication" 
+      end.
+
 
 Lemma ex_6_19 P : (~~P) <-> P. 
 P_with_CP.
@@ -218,7 +223,7 @@ Lemma ex_6_20 A B: (A \/ B) -> (~B -> A).
 P_with_CP.
 P_with_CP.
 Pose (~A) For A.
-DS H1 H.
+DS H1 H4.
 Contr H2 H3.
 Qed.
 
@@ -226,11 +231,54 @@ Lemma ex_6_21 (A B: Prop): A -> (B\/A).
 P_with_CP.
 add1 H1 B.
 Pose (~A) For (A). 
-DS H2 H.
+DS H2 H4.
 add1 H3 A.
 IP.
 Qed.
 
+Lemma ex_6_22 (A B:Prop): (A -> B) -> (~B -> ~A).  
+  Universal_Intros.
+  P_with_CP.
+  P_with_CP.
+  Pose(~~A) For (~A). 
+  DN H4.
+  MP H1 H4.
+  Contr H2 H3.
+Qed.
+
+Lemma ex_6_23 (A B C:Prop): (A\/B) -> ((A -> C) -> ((B -> C) -> C)).
+Universal_Intros. 
+P_with_CP.
+P_with_CP.
+P_with_CP.
+Pose (~C) For (C).
+MT H2 H5.
+DS H1 H5. 
+MP H3 H4. 
+IP.
+Qed.
+
+Lemma ex_6_24 (A B C: Prop) : (A -> B) -> ((B -> C) -> (A -> C)).
+Universal_Intros.
+P_with_CP.
+P_with_CP.
+P_with_CP.
+MP H1 H3. 
+MP H2 H4.
+IP.
+Qed.
+
+Lemma ex_6_25 (A B D C: Prop) : (A\/B) -> ((A -> C) -> ((B -> D) -> (C\/D))).
+  Universal_Intros.
+  P_with_CP.
+  P_with_CP.
+  P_with_CP.
+  (* We'll need a tactic to allow the reasoning by cases here *)
+  (* Pose A For (A-> (C\/D)). *)
+  destruct H1. MP H2 H. add1 H1 D. IP.
+               MP H3 H. add2 H1 C. IP.
+Qed.  
+  
 Lemma p374_6_8 A B C: (A \/ B) /\ (A \/ C) /\ ~A -> B /\ C.
 P_with_CP.
 P_with_CP.
@@ -254,17 +302,19 @@ Qed.
 Lemma ex_6_28 P Q: (~P\/Q) <-> (P -> Q).
 Universal_Intros.
 Split_Eq.
-P_with_CP.
-P_with_CP.
-DS H1 H2.
-P_with_CP.
-(* Here is where the book starts to get it wrong, I have patched below*)
-(* Pose(~(~P\/Q)) For (~P\/Q). *)
-(* Pose (~P) For P. *)
-(* DS H H2. *)
-Pose (~P) For P.
-MP H1 H.
-IP.
-bwd_Add.
-IP.
+  (* Case -> *)
+  P_with_CP.
+  P_with_CP.
+  DS H1 H2.
+  (* Case <- *)
+  P_with_CP.
+  (* Here is where the book starts to get it wrong, I have patched below*)
+  (* Pose(~(~P\/Q)) For (~P\/Q). *)
+  (* Pose (~P) For P. *)
+  (* DS H H2. *)
+  Pose (~P) For P.
+  MP H1 H.
+  IP.
+  bwd_Add.
+  IP.
 Qed.
