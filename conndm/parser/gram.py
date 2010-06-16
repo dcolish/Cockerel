8daa06708ebbf10d542b1dd24758931a22685ae8
Dan <dcolish@gmail.com>
2010-06-16 08:57:50 -0700

    snapshot of parser work

--- conndm/parser/gram.py
+++ conndm/parser/gram.py
@@ -1,51 +1,64 @@
-from ply import lex, yacc
+from ply import yacc
 
 import lexer
 from lexer import tokens
 
 
-def p_stmt(p):
-    'stmt : expr GOALINE expr'''
-    p[0] = p[1] + ' ' + p[3]
+# XXX:dc:This whole grammar needs to be reworked
+
+# def p_stmt(p):
+#     '''stmt : proofstate
+#             | theorem'''
+#     p[0] = p[1]
 
 
-def p_expr(p):
-    '''expr : termlist
-            | subgoal termlist
-            | subgoal'''
-    if len(p) == 3:
-        p[0] = p[1] + ' ' + p[2]
-    else:
-        p[0] = p[1]
+# def p_proofstate(p):
+#     '''proofstate : expr GOALINE expr'''
+#     p[0] = p[1] + ' ' + p[2] + ' ' + p[4]
 
 
-def p_subgoal(p):
-    'subgoal : NUMBER SUBGOAL'
-    p[0] = 'subgoal: ' + str(p[1])
+# def p_theorem(p):
+#     '''theorem : THEOREM expr'''
+#     p[0] = p[2]
 
 
-def p_seplist(p):
-    '''seplist : SEP seplist
-               | SEP'''
-    if len(p) == 3:
-        p[0] = p[1] + p[2]
-    else:
-        p[0] = p[1]
+# def p_expr(p):
+#     '''expr : termlist
+#             | subgoal termlist
+#             | subgoal'''
+#     if len(p) == 3:
+#         p[0] = p[1] + ' ' + p[2]
+#     else:
+#         p[0] = p[1]
 
 
-def p_termlist(p):
-    '''termlist : TERM termlist
-                | seplist termlist
-                | TERM'''
-    if len(p) == 3:
-        p[0] = p[1] + ' ' + p[2]
-    else:
-        p[0] = p[1]
+# def p_subgoal(p):
+#     'subgoal : NUMBER SUBGOAL'
+#     p[0] = 'subgoal: ' + str(p[1])
 
 
-def p_error(p):
-    print p
-    print "Syntax error in input!"
+# def p_seplist(p):
+#     '''seplist : SEP seplist
+#                | SEP'''
+#     if len(p) == 3:
+#         p[0] = p[1] + p[2]
+#     else:
+#         p[0] = p[1]
+
+
+# def p_termlist(p):
+#     '''termlist : TERM termlist
+#                 | seplist termlist
+#                 | TERM'''
+#     if len(p) == 3:
+#         p[0] = p[1] + ' ' + p[2]
+#     else:
+#         p[0] = p[1]
+
+
+# def p_error(p):
+#     print p
+#     print "Syntax error in input!"
 
 
 parser = yacc.yacc(debug=True)
--- conndm/parser/lexer.py
+++ conndm/parser/lexer.py
@@ -1,6 +1,9 @@
 from ply import lex
 
 
+t_ignore = ' \t'
+
+
 reserved = {'Theorem': 'THEOREM',
             'Goal': 'GOAL',
             'Proof': 'PROOF',
@@ -35,24 +38,15 @@ tokens = ('BSLSH',
           'RPAREN',
           'SCOL',
           'SEP',
-          'SUBGOAL',
           'TERM',
           'TILDE',
           ) + tuple(reserved.values())
 
 
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'[a-zA-Z_][a-zA-Z_0-9\']*'
     t.type = reserved.get(t.value, 'ID')    # Check for reserved words
     return t
-
-
-t_ignore = ' \t\n'
-
-
-def t_error(t):
-    print "Illegal character '%s'" % t.value[0]
-    t.lexer.skip(1)
 
 
 def t_NUMBER(t):
@@ -66,6 +60,15 @@ def t_GOALINE(t):
     t.value = str(t.value)
     return t
 
+
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 
 t_BSLSH = r'/'
@@ -89,44 +92,24 @@ t_RPAREN = r'\)'
 t_SCOL = r'\;'
 t_TILDE = r'~'
 
-
-
 lex.lex()
 
-lex.input("""
-Theorem foo (A:Prop) : A -> ~~A.
 
- 
+if __name__ == '__main__':
+
+    # A little unit test for the lexer
+
+    lex.input("""
 1 subgoal
-  
+
   ============================
    forall A : Prop, A -> ~ ~ A
 
-<prompt>foo < 2 |foo| 0 < </prompt> 
+<prompt>foo' < 2 |foo'| 0 < </prompt> 
 """)
-# 1 subgoal
-  
-#   A : Prop
-#   H : A
-#   ============================
-#    ~ ~ A
 
-# """)
-
-
-
-
-
-
-# # 1 subgoal
-  
-# #   ============================
-# #    forall A : Prop, A -> ~ ~ A
-
-# # """)
-
-while True:
-    tok = lex.token() 
-    if not tok: 
-        break
-    print tok
+    while True:
+        tok = lex.token()
+        if not tok:
+            break
+        print tok
