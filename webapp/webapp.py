cda1a1074dc13f95b13a0b50008239016ba60581
Dan <dcolish@gmail.com>
2010-06-16 21:47:07 -0700

    quick fixes

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    from pdb import set_trace; set_trace()
+    # from pdb import set_trace; set_trace()
+    proofst = None
+    lineno = int(request.args.get('line', 1))
+
     if request.method == 'POST':
         proofscript = request.form.get('f_prover_proofscript')
+        lineno += 1
         # here is where we'll pass it to conndm
-        return render_template('prover.html')
+
+
+        return render_template('prover.html', proofst=proofst,
+                               lineno=lineno)
     else:
-        return render_template('prover.html')
+        return render_template('prover.html', lineno=lineno)
 
 
 if __name__ == '__main__':
