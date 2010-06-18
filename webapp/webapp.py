283ad16fbfe4a516ec6d092015f00962b178dd12
Dan <dcolish@gmail.com>
2010-06-17 23:43:28 -0700

    added some styles to the website

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 import telnetlib
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, url_for
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -16,7 +16,8 @@ HOST = "localhost"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # from pdb import set_trace; set_trace()
+    url_for('static', filename='site.css')
+    site_css = '/static/site.css'
     proofst = None
     proofscript = "(* Begin Your Proof Here *)"
     lineno = 0
@@ -27,26 +28,34 @@ def index():
         lineno = int(request.form.get('line')) + 1
         proofscript = request.form.get('proofscript')
         flatproof = proofscript.splitlines()
-        logging.debug('Script: %s', flatproof)
         logging.debug('Sending %d : %s', lineno, flatproof[lineno])
+
         # here is where we'll pass it to coqd
         if flatproof[lineno]:
-            tn = telnetlib.Telnet(HOST, 8000)
-            tn.write(JSONEncoder().encode(dict(userid=str(session['id']),
+            try:
+                tn = telnetlib.Telnet(HOST, 8000)
+                tn.write(JSONEncoder().encode(dict(userid=str(session['id']),
                                                command=flatproof[lineno])))
-            proofst = JSONDecoder().decode(tn.read_all())
-            proofst = proofst.get('response', None)
+
+                proofst = JSONDecoder().decode(tn.read_all())
+                proofst = proofst.get('response', None)
+            except Exception:
+                logging.error("Connection to coqd failed")
+
         return render_template('prover.html',
                                proofscript=proofscript,
                                proofst=proofst,
+                               site_css=site_css,
                                lineno=lineno)
     else:
         return render_template('prover.html',
                                proofst=proofst,
                                proofscript=proofscript,
+                               site_css=site_css,
                                lineno=lineno)
 
 
 if __name__ == '__main__':
     app.debug = True
+    
     app.run()
